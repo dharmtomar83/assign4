52d4e0494f41f15ab4535ed1360c7977ad624cb7
dharm <dharm@LAPTOP-OROP1HV7>
2023-08-28 14:16:40 +0530

    Add project files.

---
+++ assign4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+static class assign4
+{
+    public static void LogMessage(string message)
+    {
+        Console.WriteLine($"[LOG] {DateTime.Now}: {message}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        assign4.LogMessage("This is a log message.");
+        assign4.LogMessage("Another log message.");
+
+        Console.WriteLine("Logging demonstration completed.");
+    }
+}
